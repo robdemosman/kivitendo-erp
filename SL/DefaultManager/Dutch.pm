ce4a67beb0f35cc4586840721e4c0f1482f83ac7
Rob Maessen <rob@localhost.localdomain>
2019-11-02 20:27:31 +0100

    rekeningsschema en vertaling

---
+++ SL/DefaultManager/Dutch.pm
@@ -0,0 +1,28 @@
+package SL::DefaultManager::Dutch;
+
+use strict;
+use parent qw(Rose::Object);
+
+# client defaults
+sub chart_of_accounts       { 'Nederlands-BTW' }
+sub accounting_method       { 'accrual' }
+sub inventory_system        { 'periodic' }
+sub profit_determination    { 'balance' }
+sub currency                { 'EUR' }
+sub precision               { 0.01 }
+sub feature_balance         { 1 }
+sub feature_datev           { 0 }
+sub feature_erfolgsrechnung { 1 }
+sub feature_eurechnung      { 0 }
+sub feature_ustva           { 1 }
+
+# user defaults
+sub numberformat            { "1 000,00" }
+sub dateformat              { 'dd.mm.yy' }
+sub timeformat              { 'hh:mm' }
+
+# default for login/admin areas
+sub country                 { 'NL' }
+sub language                { 'nl' }
+
+1;
